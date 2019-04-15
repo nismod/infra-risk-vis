9e840b397453bc813163927eeaa651172185562e
RoaldL <roald.lemmen@gmail.com>
2019-04-15 18:50:33 +0200

    Embed select feature in base map

--- src/components/maps/StaticMap.js
+++ src/components/maps/StaticMap.js
@@ -4,26 +4,27 @@ import PropTypes from 'prop-types'
 import mapboxgl from 'mapbox-gl'
 
 class StaticMap extends React.Component {
-  map
 
   constructor(props) {
     super(props);
     this.state = {
       lng: -56,
       lat: -30,
-      zoom: 3
-    };
+      zoom: 3,
+      selectedFeature: {},
+    }
     this.map = undefined
 
-    this.onLayerVisClick = this.onLayerVisClick.bind(this);
+    this.onLayerVisClick = this.onLayerVisClick.bind(this)
+    this.onFeatureHighlight = this.onFeatureHighlight.bind(this);
   }
 
   componentDidMount() {
     const { lng, lat, zoom } = this.state
 
     this.map = new mapboxgl.Map({
-        container: this.mapContainer,
-        style: this.props.style,
+      container: this.mapContainer,
+      style: this.props.style,
       center: [lng, lat],
       zoom
     })
@@ -35,6 +36,47 @@ class StaticMap extends React.Component {
         lat: lat.toFixed(4),
         zoom: this.map.getZoom().toFixed(2)
       })
+    })
+
+    this.map.on('mousemove', (e) => {
+      const features = this.map.queryRenderedFeatures(e.point);
+      this.map.getCanvas().style.cursor = ''
+      for (var i in features) {
+        if (Object.keys(this.props.clickableLayerAttributes).includes(features[i]['layer']['id'])) {
+          this.map.getCanvas().style.cursor = 'pointer'
+        }
+      }
+    });
+
+    this.map.on('click', (e) => {
+      const features = this.map.queryRenderedFeatures(e.point);
+      for (var i in features) {
+        if (Object.keys(this.props.clickableLayerAttributes).includes(features[i]['layer']['id'])) {
+          if (typeof this.map.getLayer('featureHighlight') !== "undefined" ){         
+              this.map.removeLayer('featureHighlight')
+              this.map.removeSource('featureHighlight');   
+          }
+    
+          this.map.addSource('featureHighlight', {
+              "type":"geojson",
+              "data": features[i].toJSON()
+          });
+          this.map.addLayer({
+              "id": "featureHighlight",
+              "type": "line",
+              "source": "featureHighlight",
+              "layout": {
+                  "line-join": "round",
+                  "line-cap": "round"
+              },
+              "paint": {
+                  "line-color": "yellow",
+                  "line-width": 8
+              }
+          });
+          this.onFeatureHighlight(features[i])
+        }
+      }
     })
   }
 
@@ -50,15 +92,36 @@ class StaticMap extends React.Component {
     }
   }
 
+  onFeatureHighlight(feature) {
+    const { clickableLayerAttributes } = this.props
+
+    let attributes_names = clickableLayerAttributes[feature['layer']['id']]
+    let dfeature = {}
+
+    let attributes = Object.keys(attributes_names)
+    for (var i in attributes) {
+      
+      if (attributes[i].startsWith('_')) {
+        dfeature[attributes[i]] = attributes_names[attributes[i]]
+      } else {
+        dfeature[attributes_names[attributes[i]]] = feature['properties'][attributes[i]]
+      }
+    }
+
+    this.setState({
+      selectedFeature: dfeature
+    })    
+  }
+
   render() {
-    const { lng, lat, zoom } = this.state
+    const { lng, lat, zoom, selectedFeature } = this.state
     const { toggleableLayerIds } = this.props
 
     return (
       <div>
-        <div className="absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 txt-s txt-bold">
+        <div className="absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round txt-s">
           <div className="mt6 mb12">
-            <div>Layers</div>
+            <div className="txt-bold">Layers</div>
             {
               toggleableLayerIds.map(layer => {
                 return (
@@ -77,6 +140,30 @@ class StaticMap extends React.Component {
             }
           </div>
         </div>
+
+        <div className="absolute top right mt12 mr12 bg-darken75 color-white z1 py6 px12 round txt-s">
+          <div className="mt6 mb12">
+            <div className="txt-bold">Selected Feature</div>
+            <div>{selectedFeature['_header']}</div>
+            {
+              Object.keys(selectedFeature).map(i => {
+                if (!i.startsWith('_')) {
+                  return (
+                    <div>
+                      {i}: {selectedFeature[i]}
+                    </div>
+                  ) 
+                } else {
+                  return(
+                    null
+                  )
+                }
+              })
+            }
+            
+          </div>
+        </div>
+
         <div className="absolute bottom right mb12 mr12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
           <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
         </div>
@@ -88,7 +175,8 @@ class StaticMap extends React.Component {
 
 StaticMap.propTypes = {
   style: PropTypes.string.isRequired,
-  toggleableLayerIds: PropTypes.array
+  toggleableLayerIds: PropTypes.array,
+  clickableLayerAttributes: PropTypes.object
 }
 
 export default StaticMap
--- src/containers/SimpleMap.js
+++ src/containers/SimpleMap.js
@@ -7,7 +7,30 @@ const SimpleMap = ({ style }) => (
         <div className="col-sm-12">
             <StaticMap 
                 style={"http://localhost:8080/styles/" + style + "/style.json"}
-                toggleableLayerIds={['road_secondary_tertiary', 'road_trunk_primary', 'road_major_motorway', 'aeroway', 'waterway']}/>
+                toggleableLayerIds={['road_secondary_tertiary', 'road_trunk_primary', 'road_major_motorway', 'aeroway', 'waterway']}
+                clickableLayerAttributes={{
+                    'road_secondary_tertiary': {
+                        '_header': 'Rural Road',
+                        'gml_id': 'Name',
+                        'u_jurisdic': 'Classification'
+                    },
+                    'road_trunk_primary': {
+                        '_header': 'Provincial Road',
+                        'name': 'Name',
+                        'jurisdicti': 'Classification'
+                    },
+                    'road_major_motorway': {
+                        '_header': 'Motorway',
+                        'name': 'Name',
+                        'jurisdicti': 'Classification'
+                    }, 
+                    'waterway': {},
+                    'aeroway': {
+                        '_header': 'Flight path',
+                        'from_iata': 'From', 
+                        'to_iata': 'To'
+                    }
+                }}/>
         </div>
     </div>
 )
