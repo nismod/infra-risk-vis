b7de512e676a52a4feeb399baaac56d4944a6c01
Tom Russell <tomalrussell@gmail.com>
2019-05-17 14:07:28 +0100

    Fix router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
@@ -11,42 +11,44 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => (
   <Router>
-    <Switch>
+    <Fragment>
       <Route path="/" component={Nav}/>
-      <Route  path="/overview">
-        <StaticMap
-          map_style={"http://localhost:8080/styles/overview/style.json"}
-          toggleableLayerIds={['road_secondary_tertiary', 'road_trunk_primary', 'road_major_motorway', 'aeroway', 'waterway']}
-          clickableLayerAttributes={{
-            'road_secondary_tertiary': {
-              '_header': 'Rural Road',
-              'gml_id': 'Name',
-              'u_jurisdic': 'Classification'
-            },
-            'road_trunk_primary': {
-              '_header': 'Provincial Road',
-              'name': 'Name',
-              'jurisdicti': 'Classification'
-            },
-            'road_major_motorway': {
-              '_header': 'Motorway',
-              'name': 'Name',
-              'jurisdicti': 'Classification'
-            },
-            'waterway': {},
-            'aeroway': {
-              '_header': 'Flight path',
-              'from_iata': 'From',
-              'to_iata': 'To'
-            }
-          }}/>
-        </Route>
-        <Route path="/flood">
-          <TooltipMap
-            map_style={"http://localhost:8080/styles/flood/style.json"}
-            tooltipLayerSources={['flood']}/>
-        </Route>
-    </Switch>
+      <Switch>
+        <Route path="/overview">
+          <StaticMap
+            map_style={"http://localhost:8080/styles/overview/style.json"}
+            toggleableLayerIds={['road_secondary_tertiary', 'road_trunk_primary', 'road_major_motorway', 'aeroway', 'waterway']}
+            clickableLayerAttributes={{
+              'road_secondary_tertiary': {
+                '_header': 'Rural Road',
+                'gml_id': 'Name',
+                'u_jurisdic': 'Classification'
+              },
+              'road_trunk_primary': {
+                '_header': 'Provincial Road',
+                'name': 'Name',
+                'jurisdicti': 'Classification'
+              },
+              'road_major_motorway': {
+                '_header': 'Motorway',
+                'name': 'Name',
+                'jurisdicti': 'Classification'
+              },
+              'waterway': {},
+              'aeroway': {
+                '_header': 'Flight path',
+                'from_iata': 'From',
+                'to_iata': 'To'
+              }
+            }}/>
+          </Route>
+          <Route path="/flood">
+            <TooltipMap
+              map_style={"http://localhost:8080/styles/flood/style.json"}
+              tooltipLayerSources={['flood']}/>
+          </Route>
+      </Switch>
+    </Fragment>
   </Router>
 )
 
