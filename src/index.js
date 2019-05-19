e7d98a17473bc724563b772f11c44c082ab0d2fd
Tom Russell <tomalrussell@gmail.com>
2019-05-19 17:01:00 +0100

    Add intro text

--- src/index.js
+++ src/index.js
@@ -22,12 +22,23 @@ const App = () => (
             dataSources={['road', 'air', 'water']}
             dataLayers={['road', 'air', 'water']}
             />
-          </Route>
-          <Route path="/flood">
-            <TooltipMap
-              map_style={"http://localhost:8080/styles/flood/style.json"}
-              tooltipLayerSources={['flood']}/>
-          </Route>
+        </Route>
+        <Route path="/flood">
+          <TooltipMap
+            map_style={"http://localhost:8080/styles/flood/style.json"}
+            tooltipLayerSources={['flood']}/>
+        </Route>
+        <Route path="/">
+          <div className="jumbotron welcome-float">
+            <h1 className="h1">Argentina Transport Risk Analysis</h1>
+            <p className="lead">
+            This tool presents results of the modelling and analysis of climate-related risks to transport networks in Argentina.
+            </p>
+            <p className="lead">
+            The modelling and analysis aim to support decision-making by identifying the performance of adaptation options under current and future scenarios. It comprises a network flow model, generation of failure scenarios, economic impact assessment, and cost-benefit analysis of adaptation options.
+            </p>
+          </div>
+        </Route>
       </Switch>
       </main>
     </Fragment>
