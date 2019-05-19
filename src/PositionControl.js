4a344d6f7e42e7f908925050aa81e92d2a3773c4
Tom Russell <tomalrussell@gmail.com>
2019-05-19 16:32:28 +0100

    Fix text spacing

--- src/PositionControl.js
+++ src/PositionControl.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const PositionControl = (props) => (
   <div className="custom-map-control bottom-right">
-    Longitude: {props.lng.toFixed(2)}
-    Latitude: {props.lat.toFixed(2)}
-    Zoom: {props.zoom.toFixed(0)}
+    Lon: {props.lng.toFixed(2)} Lat: {props.lat.toFixed(2)} Zoom: {props.zoom.toFixed(0)}
   </div>
 )
 
