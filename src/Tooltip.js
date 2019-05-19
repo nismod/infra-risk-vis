f5eabed3c071237354ec3612bd9021e9ebc9e2d8
Tom Russell <tomalrussell@gmail.com>
2019-05-19 16:24:49 +0100

    No  duplicate entries in tooltip

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
 const Tooltip = (props) => {
+  const seen = {};
   return (props.features.length)? (
     <div className="flex-parent-inline flex-parent--center-cross flex-parent--column absolute bottom">
       <div className="flex-child px12 py12 bg-gray-dark color-white shadow-darken10 round txt-s w240 clip txt-truncate">
         {
-          props.features.map((feature, i) => (
-            <div key={i}>
-              <strong>{feature.layer['source-layer']}</strong>
-            </div>
-          ))
+          props.features.map((feature, i) => {
+            const source_layer = feature.layer['source-layer'];
+            if (seen[source_layer]) {
+              return null;
+            }
+            seen[source_layer] = true;
+            return (
+              <div key={i}>
+                <strong>{source_layer}</strong>
+              </div>
+            )
+          })
         }
       </div>
       <span className="flex-child color-gray-dark triangle triangle--d"></span>
