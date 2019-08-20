fd4c08a0234619b74c4bfa05b2ca28ae7ec99840
Tom Russell <tomalrussell@gmail.com>
2019-08-20 13:29:38 +0100

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
 #
-# Build and deploy app
+# Deploy app
 #
 
-npm run build
+# built files for frontend
 rsync -ravz build/ raghav@argentina.oi-analytics.com:/var/www/html
+
+# data and config for tileserver
 rsync -ravz data/ raghav@argentina.oi-analytics.com:/var/www/tileserver/data
 rsync -ravz styles/ raghav@argentina.oi-analytics.com:/var/www/tileserver/styles
+rsync -ravz fonts/ raghav@argentina.oi-analytics.com:/var/www/tileserver/fonts
+rsync -ravz config.json raghav@argentina.oi-analytics.com:/var/www/tileserver
