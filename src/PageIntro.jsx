784870d151d633a42816e913515c3211ef2b0144
Tom Russell <tomalrussell@gmail.com>
2021-04-12 15:25:44 +0100

    Intro formatting

--- src/PageIntro.jsx
+++ src/PageIntro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageIntro = () => (
   <article>
-  <h1>Southeast Asia Infrastructure Risk: Prototype</h1>
+  <h1>Southeast Asia Infrastructure Risk Prototype</h1>
 
   <p>This prototype tool presents infrastructure risk analytics for South-East
   Asia (SEA) using open-data sources on fluvial and coastal flooding hazard maps
@@ -14,7 +14,7 @@ const PageIntro = () => (
   <p>The purpose of the prototype is to illustrate the type and nature of
   simulation results and network data currently available in the SEA region, and
   how &ndash; using the existing toolchain &ndash; this data might be accessed
-  and interrogated at a sub-national, national or super-national scale. </p>
+  and interrogated at a sub-national, national or super-national scale.</p>
 
   <p>The modelling and analysis presented here aim to support Disaster Risk
   Finance and Insurance (DRFI) decision-making by identifying spatial
@@ -27,21 +27,26 @@ const PageIntro = () => (
 
   <ul><li>Pant, R., Russell, T., Glasgow, G., Verschuur, J., Gavin, H., Fowler,
   T. &amp; Hall, J.W. (2021). <em>Analytics for Financial Risk Management of
-  Critical Infrastructure in South East Asia – Final Report.</em> Oxford
+  Critical Infrastructure in Southeast Asia – Final Report.</em> Oxford
   Infrastructure Analytics Ltd., Oxford, UK. (Available on request from the
   World Bank)</li></ul>
 
   <p>The tool being used to visualize the model outputs is developed and
-  documented here: </p>
+  documented here:</p>
+
+  <ul><li><a href="https://github.com/oi-analytics/oi-risk-vis"
+  target="blank">github.com/oi-analytics/oi-risk-vis</a></li></ul>
+
+  <p>The Southeast Asia analytics are produced using the code here:</p>
 
   <ul><li><a href="https://github.com/oi-analytics/seasia"
   target="blank">github.com/oi-analytics/seasia</a></li></ul>
 
   <p>The outputs specific to Vietnam visualized here were generated from a model
-  created and documented here: </p>
+  created and documented here:</p>
 
   <ul><li><a href="https://github.com/oi-analytics/vietnam-transport"
-  target="blank">Vietnam Transport Study</a></li></ul>
+  target="blank">github.com/oi-analytics/vietnam-transport</a></li></ul>
 
   <h2>Funding support</h2>
 
@@ -49,7 +54,7 @@ const PageIntro = () => (
   (DRFIP) of the World Bank with support from the Japan&mdash;World Bank Program
   for Mainstreaming DRM in Developing Countries, which is financed by the
   Government of Japan and managed by the Global Facility for Disaster Reduction
-  and Recovery (GFDRR) through the Tokyo Disaster Risk Management Hub. </p>
+  and Recovery (GFDRR) through the Tokyo Disaster Risk Management Hub.</p>
 
   </article>
 );
