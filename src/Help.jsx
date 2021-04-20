ee8c45d87475cd8086558448200d37ec85381d3c
Tom Russell <tomalrussell@gmail.com>
2021-04-20 14:17:22 +0100

    Add rel="noopener noreferrer" to links with target="_blank"

--- src/Help.jsx
+++ src/Help.jsx
@@ -37,7 +37,7 @@ const HazardHelp = () => (
       </thead>
       <tbody>
         <tr>
-          <td>Fluvial (river) flooding, from <a target="_blank" href="https://www.wri.org/data/aqueduct-floods-hazard-maps">WRI Aqueduct</a></td>
+          <td>Fluvial (river) flooding, from <a target="_blank" rel="noopener noreferrer" href="https://www.wri.org/data/aqueduct-floods-hazard-maps">WRI Aqueduct</a></td>
           <td rowSpan="2">1/2, 1/5, 1/10, 1/25, 1/50, 1/100, 1/250, 1/500, and 1/1000 </td>
           <td rowSpan="2">Flood depths in meters over 30 arc second grid squares. </td>
           <td rowSpan="2">
@@ -49,10 +49,10 @@ const HazardHelp = () => (
           </td>
         </tr>
         <tr>
-          <td>Coastal flooding with subsidence (median value), from <a target="_blank" href="https://www.wri.org/data/aqueduct-floods-hazard-maps">WRI Aqueduct</a></td>
+          <td>Coastal flooding with subsidence (median value), from <a target="_blank" rel="noopener noreferrer" href="https://www.wri.org/data/aqueduct-floods-hazard-maps">WRI Aqueduct</a></td>
         </tr>
         <tr>
-          <td>Cyclones, from <a target="_blank" href="https://data.4tu.nl/articles/dataset/STORM_tropical_cyclone_wind_speed_return_periods/12705164/2">STORM IBTrACS model</a></td>
+          <td>Cyclones, from <a target="_blank" rel="noopener noreferrer" href="https://data.4tu.nl/articles/dataset/STORM_tropical_cyclone_wind_speed_return_periods/12705164/2">STORM IBTrACS model</a></td>
           <td>28 different probabilities from 1/10 to 1/10000</td>
           <td>3-hour time step wind gust speeds in m/s at 0.1-degree grid squares.</td>
           <td>None</td>
@@ -111,7 +111,7 @@ const VietnamHelp = () => (
     Details of the study are here: Oh, J.E., Espinet Alegre, X., Pant, R., Koks,
     E.E., Russell, T., Schoenmakers, R. and Hall, J.W. (2019). Addressing
     Climate Change in Transport: Volume 2: Pathway to Resilient Transport. World
-    Bank, Washington DC. DOI: <a target="_blank" href="http://dx.doi.org/10.1596/32412">10.1596/32412</a>.
+    Bank, Washington DC. DOI: <a target="_blank" rel="noopener noreferrer" href="http://dx.doi.org/10.1596/32412">10.1596/32412</a>.
     </p>
 
     <p>
