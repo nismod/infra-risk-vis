556ca9be2c34f499cb8e52484f5cc1e92ff04b58
Tom Russell <tomalrussell@gmail.com>
2021-05-12 19:10:10 +0100

    Draft intro for Jamaica

--- src/PageIntro.jsx
+++ src/PageIntro.jsx
@@ -2,25 +2,15 @@ import React from 'react';
 
 const PageIntro = () => (
   <article>
-  <h1>Southeast Asia Infrastructure Risk Prototype</h1>
+  <h1>Jamaica Infrastructure Risk Assessment Tool</h1>
 
-  <p>This prototype tool presents infrastructure risk analytics for South-East
-  Asia (SEA) using open-data sources on fluvial and coastal flooding hazard maps
-  along with cyclone hazard maps. The risks are analysed and visualised for
-  power plants, electricity transmission lines, road networks, railway networks,
-  ports and airports in SEA. The analysis has been undertaken for the World Bank
-  by Oxford Infrastructure Analytics Ltd.</p>
+  <p>This prototype tool presents infrastructure risk analytics for Jamaica.</p>
 
-  <p>The purpose of the prototype is to illustrate the type and nature of
-  simulation results and network data currently available in the SEA region, and
-  how &ndash; using the existing toolchain &ndash; this data might be accessed
-  and interrogated at a sub-national, national or super-national scale.</p>
-
-  <p>The modelling and analysis presented here aim to support Disaster Risk
-  Finance and Insurance (DRFI) decision-making by identifying spatial
-  criticalities and risks under current and future hazard scenarios. It
-  comprises a direct damage estimation and an indirect economic loss estimation
-  of GDP disruptions due to asset failures and service disruption.</p>
+  <p>The modelling and analysis presented here aim to support climate adaptation
+  decision-making by identifying spatial criticalities and risks under current
+  and future hazard scenarios. It comprises a direct damage estimation and an
+  indirect economic loss estimation of GDP disruptions due to asset failures and
+  service disruption.</p>
 
   <table className="table table-sm table-striped">
     <thead>
@@ -33,68 +23,51 @@ const PageIntro = () => (
     </thead>
     <tbody>
       <tr>
-        <td>Road</td>
+        <td rowspan="2">Transport</td>
         <td>Road links</td>
         <td>Cost of rehabilitation/reinstating damaged assets</td>
-        <td>Rerouting costs* + Macroeconomic losses**</td>
+        <td>Rerouting costs + wider effects of service disruption</td>
       </tr>
       <tr>
-        <td>Rail</td>
         <td>Railway tracks</td>
         <td>Cost of rehabilitation/reinstating damaged assets</td>
-        <td>Rerouting costs* + Macroeconomic losses**</td>
+        <td>Rerouting costs + wider effects of service disruption</td>
       </tr>
       <tr>
-        <td>Electricity</td>
-        <td>Electricity lines</td>
+        <td>Energy</td>
+        <td>Electricity transmission and distribution grid: generation, lines and substations</td>
         <td>Cost of rehabilitation/reinstating damaged assets</td>
-        <td>Macroeconomic losses**</td>
+        <td>Wider effects of service disruption</td>
+      </tr>
+      <tr>
+        <td>Water</td>
+        <td>Water supply and wastewater networks, abstraction points, irrigation schemes</td>
+        <td>Cost of rehabilitation/reinstating damaged assets</td>
+        <td>Wider effects of service disruption</td>
       </tr>
     </tbody>
   </table>
 
-  <p><small><em>* Rerouting costs are computed for the Vietnam case study only and
-  ignored for other regions. The pages showing results for all of Southeast Asia
-  do not include the rerouting costs for Vietnam.</em></small></p>
+  <p>This tool to visualize the model outputs is developed and documented
+  here:</p>
 
-  <p><small><em>** Macroeconomic losses are computed based on GDP for all regions in the
-  Southeast Asia analysis. In the Vietnam case study, IO models were used to
-  estimate wider effects of disruption on the macroeconomy - these results are
-  shown only on the Vietnam case study page.</em></small></p>
+  <ul><li><a href="https://github.com/nismod/infra-risk-vis"
+  target="blank">github.com/nismod/infra-risk-vis</a></li></ul>
 
-  <p>The concepts and model results presented here are documented in the study
-  report:</p>
+  <p>The analytics for Jamaica are produced using the code and models here:</p>
 
-  <ul><li>Pant, R., Russell, T., Glasgow, G., Verschuur, J., Gavin, H., Fowler,
-  T. &amp; Hall, J.W. (2021). <em>Analytics for Financial Risk Management of
-  Critical Infrastructure in Southeast Asia – Final Report.</em> Oxford
-  Infrastructure Analytics Ltd., Oxford, UK. (Available on request from the
-  World Bank)</li></ul>
-
-  <p>The tool being used to visualize the model outputs is developed and
-  documented here:</p>
-
-  <ul><li><a href="https://github.com/oi-analytics/oi-risk-vis"
-  target="blank">github.com/oi-analytics/oi-risk-vis</a></li></ul>
-
-  <p>The Southeast Asia analytics are produced using the code here:</p>
-
-  <ul><li><a href="https://github.com/oi-analytics/seasia"
-  target="blank">github.com/oi-analytics/seasia</a></li></ul>
-
-  <p>The outputs specific to Vietnam visualized here were generated from a model
-  created and documented here:</p>
-
-  <ul><li><a href="https://github.com/oi-analytics/vietnam-transport"
-  target="blank">github.com/oi-analytics/vietnam-transport</a></li></ul>
+  <ul><li><a href="https://github.com/nismod/jamaica-infrastructure"
+  target="blank">github.com/nismod/jamaica-infrastructure</a></li></ul>
 
   <h2>Funding support</h2>
 
-  <p>This project is led by the Disaster Risk Financing and Insurance Program
-  (DRFIP) of the World Bank with support from the Japan&mdash;World Bank Program
-  for Mainstreaming DRM in Developing Countries, which is financed by the
-  Government of Japan and managed by the Global Facility for Disaster Reduction
-  and Recovery (GFDRR) through the Tokyo Disaster Risk Management Hub.</p>
+  <p>This research is led by researchers in the Oxford Programme for Sustainable
+  Infrastructure Systems in the Environmental Change Institute, University of
+  Oxford, for the Government of Jamaica (GoJ) as part of a project funded by UK
+  Aid (FCDO). The initiative forms part of the Coalition for Climate Resilient
+  Investment’s (CCRI) collaboration with the GoJ, which includes analysis of
+  nature-based approaches to build resilience in Jamaica to be procured and
+  funded by the Green Climate Fund (GCF).</p>
 
   </article>
 );
