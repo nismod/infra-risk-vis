9d35481782241d902bafdb2b754d0acdb91b8050
Tom Russell <tomalrussell@gmail.com>
2021-05-12 19:09:57 +0100

    Drop OIA logo

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,9 +3,6 @@ import { NavLink } from 'react-router-dom'
 
 const Nav = () => (
   <nav className="navbar navbar-height navbar-expand navbar-light">
-    <NavLink className="navbar-brand" to="/">
-      <img src="/logo.png" alt="OIA" />
-    </NavLink>
     <ul className="navbar-nav mr-auto">
       <li className="nav-item">
         <NavLink exact className="nav-link" to='/'>
