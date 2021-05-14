07241084097d4622a17d726c87809080c9c3333b
Tom Russell <tomalrussell@gmail.com>
2021-05-14 17:06:33 +0100

    No need for makeStyles

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles();
-
 const Nav = () => {
-  const classes = useStyles();
   return (
     <AppBar position="fixed">
       <Toolbar>
-        <Typography variant="h6" className={classes.title}>
+        <Typography variant="h6">
           <span className="nav-brand">
           Infrastructure Risk Assessment
           </span>
         </Typography>
         <NavLink exact className="nav-link" to='/'>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6">
             About
           </Typography>
         </NavLink>
         <NavLink className="nav-link" to='/overview'>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6">
             Infrastructure networks
           </Typography>
         </NavLink>
