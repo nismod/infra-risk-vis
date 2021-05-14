fb66b3720320e5f6adaf2081002b837e0e52c2f4
Tom Russell <tomalrussell@gmail.com>
2021-05-14 17:53:40 +0100

    Plot layers

--- src/App.jsx
+++ src/App.jsx
@@ -23,26 +23,29 @@ const App = () => (
         <Map
           map_style="overview"
           dataSources={[
-            'electricity',
-            'rail',
-            'roads_main',
-            'roads_other'
+            'road_edges',
+            'bridges',
+            'rail_edges',
+            'rail_nodes',
+            'elec_edges',
+            'elec_nodes'
           ]}
           dataLayers={[
-            {key: 'electricity', label: 'Power Grid', linear: true, color: "#eca926"},
-            {key: 'rail', label: 'Railways', linear: true, color: "#444"},
-            {key: 'trunk', label: 'Trunk Roads', linear: true, color: "#941339"},
-            {key: 'motorway', label: 'Motorways', linear: true, color: "#941339"},
-            {key: 'primary', label: 'Primary Roads', linear: true, color: "#cb3e4e"},
-            {key: 'secondary', label: 'Secondary Roads', linear: true, color: "#8471a8"},
-            {key: 'roads_other', label: 'Tertiary and Other Roads', linear: true, color: "#b2afaa"},
-
+            {key: 'road_edges', label: 'Roads', linear: true, color: "#b2afaa"},
+            {key: 'bridges', label: 'Bridges', color: "#487dbc"},
+            {key: 'rail_edges', label: 'Railways', linear: true, color: "#444"},
+            {key: 'rail_nodes', label: 'Stations', color: "#444"},
+            {key: 'elec_edges_high', label: 'Power Lines (High Voltage)', linear: true, color: "#eca926"},
+            {key: 'elec_edges_low', label: 'Power Lines (Low Voltage)', linear: true, color: "#f1d75c"},
+            {key: 'elec_nodes', label: 'Power Nodes', color: "#eca926"},
           ]}
           tooltipLayerSources={[
-            'electricity',
-            'rail',
-            'roads_main',
-            'roads_other'
+            'road_edges',
+            'bridges',
+            'rail_edges',
+            'rail_nodes',
+            'elec_edges',
+            'elec_nodes'
           ]}
           />
       </Route>
