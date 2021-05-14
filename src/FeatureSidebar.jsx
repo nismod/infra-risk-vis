b5957fb005e013b1d4b5f511b01efd447bcdb08a
Tom Russell <tomalrussell@gmail.com>
2021-05-14 22:25:52 +0100

    Demo time series for one point

--- src/FeatureSidebar.jsx
+++ src/FeatureSidebar.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
+import  { VegaLite } from 'react-vega';
 
 import { titleCase } from './helpers';
 
@@ -31,6 +32,37 @@ const FeatureSidebar = (props) => {
           ))
         }
       </List>
+      {
+        (f.node_id === 'Pump_682')?
+          <div style={{paddingLeft:'10px'}}>
+          <Typography variant="caption">Stream Flow</Typography>
+          <br/>
+          <VegaLite
+            spec={{
+              width: 400,
+              height: 200,
+              mark: 'line',
+              encoding: {
+                x: { field: 'date', type: 'temporal', title: 'Date' },
+                y: {
+                  field: 'flow_1000m3_per_day',
+                  type: 'quantitative',
+                  title: 'Stream Flow (1000 m³ per day)'
+                }
+              },
+              data: { url: 'flow_cave_river.csv' },
+            }}
+            // defines the actions available behind the ... menu top-right of chart
+            actions={{
+              export: true,
+              source: false,
+              compiled: false,
+              editor: false,
+            }}
+          />
+          </div>
+        : null
+      }
     </div>
   )
 }
