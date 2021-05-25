b48d203b1e992f73d5a03b6edf9c429e1f6cd576
Tom Russell <tomalrussell@gmail.com>
2021-05-25 10:32:27 +0100

    Drop unused import

--- src/FeatureSidebar.jsx
+++ src/FeatureSidebar.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
