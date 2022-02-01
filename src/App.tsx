4833989438467e59119610ecc4fc11767de7d378
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-02-01 19:54:59 +0100

    Add default MUI ThemeProvider
    Following migration to MUI v5 described at the link:
    https://mui.com/guides/migration-v4/#themeprovider-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import { CssBaseline, Toolbar } from '@material-ui/core';
+import { createMuiTheme, CssBaseline, ThemeProvider, Toolbar } from '@material-ui/core';
 
 import { Nav } from './Nav';
 import { IntroPage } from './IntroPage';
@@ -11,20 +11,27 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { ViewName } from './config/views';
 import { RecoilRoot } from 'recoil';
 
+const theme = createMuiTheme();
+
 export const App = () => {
   return (
     <RecoilRoot>
-    <Router>
-      <CssBaseline />
-      <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <Toolbar /> {/* Prevents app bar from concealing content*/}
-          <IntroPage />
-        </Route>
-        <Route path="/:view(overview)" render={({ match: { params } }) => <MapPage view={params.view as ViewName} />} />
-      </Switch>
-    </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <CssBaseline />
+          <Nav />
+          <Switch>
+            <Route path="/" exact>
+              <Toolbar /> {/* Prevents app bar from concealing content*/}
+              <IntroPage />
+            </Route>
+            <Route
+              path="/:view(overview)"
+              render={({ match: { params } }) => <MapPage view={params.view as ViewName} />}
+            />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </RecoilRoot>
   );
 };
