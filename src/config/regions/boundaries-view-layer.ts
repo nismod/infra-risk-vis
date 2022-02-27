03eefb0465c75da1c81093b2d8c15b7f6f6ad76f
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-02-27 22:00:07 +0000

    Fix layer id-related tooltip bug

--- src/config/regions/boundaries-view-layer.ts
+++ src/config/regions/boundaries-view-layer.ts
@@ -1,9 +1,10 @@
 import { regionBoundariesDeckLayer } from './region-boundaries-deck-layer';
 import { RegionLevel } from './metadata';
+import { ViewLayer } from 'lib/data-map/view-layers';
 
-export function regionBoundariesViewLayer(regionLevel: RegionLevel) {
+export function regionBoundariesViewLayer(regionLevel: RegionLevel): ViewLayer {
   return {
-    id: `region_boundaries_${regionLevel}`,
+    id: `boundaries_${regionLevel}`,
     group: 'regions',
     spatialType: 'vector',
     interactionGroup: 'regions',
