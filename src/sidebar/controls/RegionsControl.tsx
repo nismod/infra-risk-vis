4cde9be3f5ed5be2ba9b14f26ca978b51b782201
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-03-03 19:09:40 +0100

    Switch region level UI to toggle button group

--- src/sidebar/controls/RegionsControl.tsx
+++ src/sidebar/controls/RegionsControl.tsx
@@ -1,18 +1,30 @@
-import { FormControlLabel, Radio, RadioGroup, Box, FormControl, FormLabel, Select, MenuItem } from '@mui/material';
+import { Box, FormControl, FormLabel, Select, MenuItem, ToggleButtonGroup, ToggleButton } from '@mui/material';
 import { useRecoilState } from 'recoil';
 import { RegionLevel } from 'config/regions/metadata';
 import { regionDataState, RegionDataType, regionLevelState } from 'state/regions';
+import { useCallback } from 'react';
 
 export const RegionLevelSelection = () => {
   const [regionLevel, setRegionLevel] = useRecoilState(regionLevelState);
+
+  const handleChange = useCallback(
+    (e, value: string) => {
+      if (value != null) {
+        setRegionLevel(value as RegionLevel);
+      }
+    },
+    [setRegionLevel],
+  );
+
   return (
-    <FormControl component="fieldset">
-      <FormLabel>Administrative division</FormLabel>
-      <RadioGroup value={regionLevel} onChange={(e, value) => setRegionLevel(value as RegionLevel)}>
-        <FormControlLabel value="parish" label="Parishes" control={<Radio unselectable="off" />} />
-        <FormControlLabel value="enumeration" label="Enumeration Districts" control={<Radio unselectable="off" />} />
-      </RadioGroup>
-    </FormControl>
+    <ToggleButtonGroup exclusive value={regionLevel} onChange={handleChange} fullWidth>
+      <ToggleButton value="parish" sx={{ textTransform: 'none' }}>
+        Parishes
+      </ToggleButton>
+      <ToggleButton value="enumeration" sx={{ textTransform: 'none' }}>
+        Enumeration Districts
+      </ToggleButton>
+    </ToggleButtonGroup>
   );
 };
 
