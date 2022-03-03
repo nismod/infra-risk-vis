e0e41e313ad7be15486ff5266cef21d6792b4cb2
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-03-03 18:12:37 +0100

    Fix sidebar scrollbar issue
    This solution prevents the sidebar resizing when a scrollbar appears.
    The behaviour is also more consistent between Firefox and Chrome,
    so the map legend / controls can be moved closer to the sidebar.
    One downside is that the area where the scrollbar appears
    doesn't register map events (zoom/pan).

--- src/MapPage.tsx
+++ src/MapPage.tsx
@@ -9,24 +9,40 @@ interface MapViewProps {
   view: string;
 }
 
+const SidebarLayout = ({ children }) => (
+  <Box
+    position="absolute"
+    top={globalStyleVariables.navbarHeight}
+    left={0}
+    bottom={0}
+    width={globalStyleVariables.sidebarWidth - 20}
+    zIndex={1000}
+    sx={{ pointerEvents: 'none' }}
+  >
+    <Box
+      overflow="auto"
+      maxHeight="100%"
+      sx={{ pointerEvents: 'auto' }}
+      position="absolute"
+      left={0}
+      right={-20}
+      top={0}
+    >
+      <Box width={globalStyleVariables.sidebarWidth - 20}>
+        <Box pl={1} pt={1} sx={{ pointerEvents: 'auto' }}>
+          {children}
+        </Box>
+      </Box>
+    </Box>
+  </Box>
+);
+
 export const MapPage: FC<MapViewProps> = ({ view }) => {
   return (
     <>
-      <Box
-        position="absolute"
-        top={globalStyleVariables.navbarHeight}
-        left={0}
-        bottom={0}
-        width={globalStyleVariables.sidebarWidth}
-        zIndex={1000}
-        overflow="auto"
-        boxSizing="border-box"
-        sx={{ pointerEvents: 'none' }}
-      >
-        <Box pl={1} pt={1} sx={{ pointerEvents: 'auto' }}>
-          <SidebarContent view={view} />
-        </Box>
-      </Box>
+      <SidebarLayout>
+        <SidebarContent view={view} />
+      </SidebarLayout>
       <Box position="absolute" overflow="clip" top={globalStyleVariables.navbarHeight} left={0} right={0} bottom={0}>
         <MapView view={view} />
       </Box>
