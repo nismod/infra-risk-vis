673c8a29ac70a651e8767384d246bdf05a97c143
Tom Russell <tomalrussell@gmail.com>
2022-03-16 09:27:08 +0000

    Show area for parishes/enumeration districts

--- src/details/regions/RegionDetailsContent.tsx
+++ src/details/regions/RegionDetailsContent.tsx
@@ -8,13 +8,14 @@ import { FC } from 'react';
 export const RegionDetailsContent: FC<{ selectedRegion: InteractionTarget<any> }> = ({ selectedRegion }) => {
   const metadata = REGIONS_METADATA[selectedRegion.viewLayer.params.regionLevel];
   const f = selectedRegion.target.feature.properties;
+  const area = f['AREA']? f['AREA'] * 1e-6 : f['Shape_Area'] * 1e-6;
 
   return (
     <>
       {metadata.labelSingular}
       <Typography variant="h6">{selectedRegion.target.feature.properties[metadata.fieldName]}</Typography>
       <DataItem label="Population" value={f['population'].toLocaleString()} />
-      <DataItem label="Area (km²)" value={numFormat(f['AREA'] * 1e-6)} />
+      <DataItem label="Area (km²)" value={numFormat(area)} />
       <DataItem label="Population per km²" value={numFormat(f['population_density_per_km2'])} />
     </>
   );
