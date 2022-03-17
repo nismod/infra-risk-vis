8b8290db6622920dcd6a181f7f008f395d092f0d
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-03-17 12:11:42 +0100

    Add damages to database

--- api/app/models.py
+++ api/app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Integer, Column, String, JSON
+from sqlalchemy import ForeignKey, Integer, Column, String, JSON, Float
+from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
 
 from .database import Base
@@ -7,8 +8,27 @@ from .database import Base
 class Feature(Base):
     __tablename__ = "features"
 
-    id = Column(String, primary_key=True, index=True)
-    layer = Column(String, index=True)
+    id = Column(String, primary_key=True)
+    layer = Column(String, index=True, nullable=False)
     sublayer = Column(String)
-    properties = Column(JSON)
-    geom = Column(Geometry)
+    properties = Column(JSON, nullable=False)
+    geom = Column(Geometry, nullable=False)
+
+    damages = relationship("Damage")
+
+
+class Damage(Base):
+    __tablename__ = "damages"
+
+    feature_id = Column("id", String, ForeignKey(Feature.id), primary_key=True)
+    hazard = Column(String, nullable=False, primary_key=True)
+    rcp = Column(String, nullable=False, primary_key=True)
+    epoch = Column(String, nullable=False, primary_key=True)
+
+    EAD_undefended_min = Column(Float)
+    EAD_undefended_mean = Column(Float)
+    EAD_undefended_max = Column(Float)
+
+    EAEL_undefended_min = Column(Float)
+    EAEL_undefended_mean = Column(Float)
+    EAEL_undefended_max = Column(Float)
