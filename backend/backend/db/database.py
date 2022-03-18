ee07f11c2143dd883b5a265d036931259531df0d
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-03-18 13:41:10 +0100

    Get db connection setup from env vars

--- backend/backend/db/database.py
+++ backend/backend/db/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://docker:docker@db/jamaica"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# pass empty connection string to use PG* environment variables (see https://www.postgresql.org/docs/current/libpq-envars.html)
+engine = create_engine("postgresql+psycopg2://")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
