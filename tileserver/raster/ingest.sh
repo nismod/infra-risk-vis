fc1b9291aeec5763531623de466e5a25d6d2e3fd
Tom Russell <tomalrussell@gmail.com>
2022-04-25 22:35:42 +0100

    Process hazard layers through snakemake

--- tileserver/raster/ingest.sh
+++ tileserver/raster/ingest.sh
@@ -2,14 +2,6 @@
 
 RASTER_BASE_PATH=$1
 
-terracotta optimize-rasters \
-  -o "$RASTER_BASE_PATH/data" \
-  --overwrite \
-  --reproject \
-  --nproc -1 \
-  --resampling-method nearest \
-  "$RASTER_BASE_PATH/input/*.tif"
-
 terracotta ingest \
   "$RASTER_BASE_PATH/data/{type}__rp_{rp}__rcp_{rcp}__epoch_{epoch}__conf_{confidence}.tif" \
   -o "$RASTER_BASE_PATH/terracotta.sqlite"
