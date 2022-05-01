6b6cbb6ac3f46cb34d2f5fbe7e4ffd01a42b15a6
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-05-01 04:08:00 +0200

    Scope Jamaica styling to main toolbar only

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -5,7 +5,17 @@ import { NavLink } from 'react-router-dom';
 export const Nav = () => {
   return (
     <AppBar position="fixed">
-      <Toolbar>
+      <Toolbar
+        sx={{
+          background:
+            'linear-gradient(180deg, rgba(0,128,0,1) 0%, rgba(0,128,0,1) 5%, rgba(9,9,9,1) 5%, rgba(9,9,9,1) 100%);',
+          '& a.nav-link': {
+            '&:hover,&:focus,&:active,&.active': {
+              borderBottom: '1px solid #ffd700',
+            },
+          },
+        }}
+      >
         <NavLink exact className="nav-link" to="/">
           <Typography variant="h6">J-SRAT</Typography>
         </NavLink>
