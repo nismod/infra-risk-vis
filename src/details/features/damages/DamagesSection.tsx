2398a63d2013da9fe0839af5e7412c846ee9da94
Tom Russell <tomalrussell@gmail.com>
2022-05-01 22:31:43 -0500

    Show return period damages (direct)

--- src/details/features/damages/DamagesSection.tsx
+++ src/details/features/damages/DamagesSection.tsx
@@ -2,13 +2,15 @@ import { Download } from '@mui/icons-material';
 import { FormControl, InputLabel, IconButton, MenuItem, Select, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { HAZARD_DOMAINS } from 'config/hazards/domains';
-import { ExpectedDamage } from 'lib/api-client';
+import { ExpectedDamage, ReturnPeriodDamage } from 'lib/api-client';
 import { downloadFile, titleCase, unique } from 'lib/helpers';
 import { useSelect } from 'lib/hooks/use-select';
 import _ from 'lodash';
 import { useMemo } from 'react';
 import { DamageTable } from './DamageTable';
+import { RPDamageTable } from './RPDamageTable';
 import { ExpectedDamageChart } from './ExpectedDamageChart';
+import { ReturnPeriodDamageChart } from './ReturnPeriodDamageChart';
 
 const DAMAGES_ORDERING = (() => {
   const ordering = [];
@@ -26,8 +28,30 @@ const DAMAGES_ORDERING = (() => {
   return ordering;
 })();
 
+const RP_ORDERING = (() => {
+  const ordering = [];
+  for (const [hazard, hazardDomain] of Object.entries(HAZARD_DOMAINS)) {
+    for (const rp of hazardDomain.paramDomains.returnPeriod) {
+      for (const rcp of hazardDomain.paramDomains.rcp) {
+        for (const epoch of hazardDomain.paramDomains.epoch) {
+          ordering.push({
+            hazard,
+            rcp,
+            epoch,
+            rp
+          });
+        }
+      }
+    }
+  }
+  return ordering;
+})();
+
 function getDamageKey({ hazard, rcp, epoch }) {
   return `${hazard}__rcp_${rcp}__epoch_${epoch}__conf_None`;
+}
+function getRPDamageKey({ hazard, rcp, epoch, rp }) {
+  return `${hazard}__rcp_${rcp}__epoch_${epoch}__rp_${rp}__conf_None`;
 }
 
 interface ExpectedDamageCell {
@@ -42,33 +66,59 @@ interface ExpectedDamageCell {
   eael_amin: number,
   eael_amax: number,
 }
-function getExpectedDamageObject({
-  hazard,
-  rcp,
-  epoch,
-  ead_mean,
-  ead_amin,
-  ead_amax,
-  eael_mean,
-  eael_amin,
-  eael_amax
-}: ExpectedDamage): ExpectedDamageCell {
+function getExpectedDamageObject(d: ExpectedDamage): ExpectedDamageCell {
   return {
-    key: getDamageKey({ hazard, rcp, epoch }),
-    hazard,
-    rcp,
-    epoch: epoch.toString(),
-    ead_mean,
-    ead_amin,
-    ead_amax,
-    eael_mean,
-    eael_amin,
-    eael_amax,
+    key: getDamageKey({ hazard: d.hazard, rcp: d.rcp, epoch: d.epoch }),
+    hazard: d.hazard,
+    rcp: d.rcp,
+    epoch: d.epoch.toString(),
+    ead_mean: d.ead_mean,
+    ead_amin: d.ead_amin,
+    ead_amax: d.ead_amax,
+    eael_mean: d.eael_mean,
+    eael_amin: d.eael_amin,
+    eael_amax: d.eael_amax,
+  };
+}
+
+interface RPDamageCell {
+  key: string;
+  hazard: string;
+  rcp: string;
+  rp: number;
+  probability: number;
+  epoch: string;
+  damage_mean: number,
+  damage_amin: number,
+  damage_amax: number,
+  loss_mean: number,
+  loss_amin: number,
+  loss_amax: number,
+}
+
+function getRPDamageObject(d: ReturnPeriodDamage): RPDamageCell {
+  return {
+    key: getRPDamageKey({ hazard: d.hazard, rcp: d.rcp, epoch: d.epoch, rp: d.rp }),
+    hazard: d.hazard,
+    rcp: d.rcp,
+    rp: d.rp,
+    probability: 1 / d.rp,
+    epoch: d.epoch.toString(),
+    damage_mean: d.damage_mean,
+    damage_amin: d.damage_amin,
+    damage_amax: d.damage_amax,
+    loss_mean: d.loss_mean,
+    loss_amin: d.loss_amin,
+    loss_amax: d.loss_amax,
   };
 }
 
 function prepareExpectedDamages(expectedDamages: ExpectedDamage[]) {
   return expectedDamages.filter((d) => d.protection_standard === 0).map(getExpectedDamageObject);
+}
+
+function prepareRPDamages(rpDamages: ReturnPeriodDamage[]) {
+  return rpDamages.map(getRPDamageObject);
 }
 
 function orderDamages(damages: ExpectedDamageCell[]) {
@@ -79,20 +129,41 @@ function orderDamages(damages: ExpectedDamageCell[]) {
     .filter(Boolean);
 }
 
+function orderRPDamages(damages: RPDamageCell[]) {
+  const lookup = _.fromPairs(damages.map((d) => [d.key, d]));
+
+  return RP_ORDERING.map(getRPDamageKey)
+    .map((key) => lookup[key])
+    .filter(Boolean);
+}
+
 function makeDamagesCsv(damages: ExpectedDamageCell[]) {
   return 'hazard,rcp,epoch,ead_mean,ead_amin,ead_amax,eael_mean,eael_amin,eael_amax\n' + damages.map(
     (d) => `${d.hazard},${d.rcp},${d.epoch},${d.ead_mean},${d.ead_amin},${d.ead_amax},${d.eael_mean},${d.eael_amin},${d.eael_amax}`).join('\n');
 }
 
+function makeRPDamagesCsv(damages: RPDamageCell[]) {
+  return 'hazard,rcp,epoch,rp,damage_mean,damage_amin,damage_amax,loss_mean,loss_amin,loss_amax\n' + damages.map(
+    (d) => `${d.hazard},${d.rcp},${d.epoch},${d.rp},${d.damage_mean},${d.damage_amin},${d.damage_amax},${d.loss_mean},${d.loss_amin},${d.loss_amax}`).join('\n');
+}
+
 export const DamagesSection = ({ fd }) => {
+  console.log(fd)
   const damagesData = orderDamages(prepareExpectedDamages(fd?.damages_expected ?? []));
+  const returnPeriodDamagesData = orderRPDamages(prepareRPDamages(fd?.damages_return_period ?? []));
 
   const hazards = useMemo(() => unique(damagesData.map((d) => d.hazard)), [damagesData]);
+  const epochs = useMemo(() => unique(damagesData.map((d) => d.epoch)), [damagesData]);
   const [selectedHazard, setSelectedHazard] = useSelect(hazards);
+  const [selectedEpoch, setSelectedEpoch] = useSelect(epochs);
 
   const selectedData = useMemo(
     () => (selectedHazard ? damagesData.filter((x) => x.hazard === selectedHazard) : null),
     [selectedHazard, damagesData],
+  );
+  const selectedRPData = useMemo(
+    () => (selectedHazard ? returnPeriodDamagesData.filter((x) => x.hazard === selectedHazard && x.epoch === selectedEpoch) : null),
+    [selectedHazard, selectedEpoch, returnPeriodDamagesData],
   );
 
   const has_eael = useMemo(
@@ -191,6 +262,80 @@ export const DamagesSection = ({ fd }) => {
             }
             <Box mt={1}>
               <DamageTable damages={selectedData} />
+            </Box>
+          </>
+        ) : (
+          <Typography variant="body2" color="textSecondary">
+            No direct damages or indirect losses estimated.
+          </Typography>
+        )}
+      </Box>
+      <Box py={2}>
+        <Box position="relative">
+          <Typography variant="h6">Return Period Damages</Typography>
+          <IconButton
+            sx={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+            }}
+            title="Download CSV with return period data"
+            onClick={() => downloadFile(makeRPDamagesCsv(returnPeriodDamagesData), 'text/csv', `feature_${fd.id}_damages_rp.csv`)}
+          >
+            <Download />
+          </IconButton>
+        </Box>
+        {epochs.length ? (
+          epochs.length === 1?
+            (
+              <FormControl fullWidth sx={{my:2}} disabled>
+                <InputLabel>Epoch</InputLabel>
+                <Select
+                  value={selectedEpoch ?? ''}
+                  onChange={(e) => setSelectedEpoch(e.target.value as string)}
+                  >
+                  {epochs.map((h) => (
+                    <MenuItem key={h} value={h}>
+                      {titleCase(h)}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            ) : (
+              <FormControl fullWidth sx={{my:2}}>
+                <InputLabel>Epoch</InputLabel>
+                <Select
+                  value={selectedEpoch ?? ''}
+                  onChange={(e) => setSelectedEpoch(e.target.value as string)}
+                  >
+                  {epochs.map((h) => (
+                    <MenuItem key={h} value={h}>
+                      {titleCase(h)}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )
+          ) : null
+        }
+        {selectedRPData ? (
+          <>
+            <Box mt={1}>
+              <ReturnPeriodDamageChart
+                data={{
+                  table: selectedRPData,
+                }}
+                field_key='damage_mean'
+                field_title='Damage (J$)'
+                actions={false}
+                padding={0}
+                width={260} // this is currently picked to fit the chart to the sidebar width
+                height={150}
+                renderer="svg"
+              />
+            </Box>
+            <Box mt={1}>
+              <RPDamageTable damages={selectedRPData} />
             </Box>
           </>
         ) : (
