0578946a6fd6a5b8d62998012077a11d609aa45a
Tom Russell <tomalrussell@gmail.com>
2022-07-29 17:29:38 +0100

    Add photo credit to front page

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -1,109 +1,86 @@
 import React from 'react';
-import { Alert, Button, Divider, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import { Divider, Grid, Paper, Stack, Typography } from '@mui/material';
 import ScrollToTop from 'lib/hooks/scroll-to-top';
+
+const HeadingBox = styled(Paper)(({ theme }) => ({
+  backgroundColor: 'rgba(0, 92, 97, 0.3)',
+  color: '#fff',
+  padding: theme.spacing(2),
+  borderRadius: 0,
+}));
+
+const TextBox = styled(Paper)(() => ({
+  backgroundColor: 'rgba(194, 219, 231, 0.9)',
+  color: '#333',
+  padding: '16px 32px',
+  borderRadius: 0,
+}));
 
 export const IntroPage = () => (
   <article>
     <ScrollToTop />
-    <h1>Caribbean Systemic Risk Assessment Tool</h1>
+    <Grid container columnSpacing={8} rowSpacing={4}>
+      <Grid item xs={6}>
+        <HeadingBox sx={{mt:-2, pt:8}}>
+          <Typography variant="h1">
 
-    <p>The Systemic Risk Assessment Tool (SRAT) presents
-    climate-related risk analytics for transport and energy
-    infrastructure across the Caribbean.</p>
+            Climate-related risk analytics for transport, energy &amp; water
+            infrastructure in the Caribbean
 
-    <Alert
-      severity="success"
-      action={
-        <Button color="inherit" size="small">
-          <a href="https://github.com/nismod/infra-risk-vis/issues">
-            REPORT
-          </a>
-        </Button>
-      }
-    >
+          </Typography>
+        </HeadingBox>
+      </Grid>
+      <Grid item xs={6}>
+        <TextBox sx={{mt:-2, pt:8}}>
+          <p>
+            The Systemic Risk Assessment Tool (SRAT) supports
+            climate adaptation decision-making by identifying spatial
+            criticalities and risks under current and future climate scenarios.
+          </p>
+          <Typography variant="h2">Transport</Typography>
+          <p>Roads, from major to tertiary.</p>
+          <Typography variant="h2">Energy</Typography>
+          <p>Electricity generation and transmission.</p>
 
-    The tool has recently been released. Please tell us if anything is not
-    working as it should and suggest potential improvements.
+        </TextBox>
+      </Grid>
+      <Grid item xs={12}>
+        <TextBox sx={{backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
 
-    </Alert>
+          <p>The research, analysis and development has been led by
+            researchers in the&nbsp;<a href="https://opsis.eci.ox.ac.uk/" target="_blank" rel="noopener noreferrer">Oxford Programme for
+            Sustainable Infrastructure Systems</a>, University of Oxford,
+            funded by UK Aid through the UK Foreign and
+            Commonwealth Development Office (FCDO)</p>
+          <p>
+          <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem />}
 
-    <p>The modelling and analysis presented here aim to support climate
-    adaptation decision-making by identifying spatial criticalities and risks
-    under current and future climate scenarios.</p>
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+            >
+            <a href="https://opsis.eci.ox.ac.uk" target="_blank" rel="noopener noreferrer">
+              <img height="100" src="/logo-opsis.png" alt="Oxford Programme for Sustainable Infrastructure Systems" />
+            </a>
+            <a href="https://www.gov.uk/guidance/uk-aid" target="_blank" rel="noopener noreferrer">
+              <img height="100" src="/logo-ukaid.png" alt="UK AID" />
+            </a>
+          </Stack>
+          </p>
 
-    <p>The following table summarises the data and model results presented.</p>
+          <p><small>
+            Photo credit: Hurricane Irma, 7 September 2017.
+            Data: MODIS/Terra (NASA WorldView). Processed by Antti Lipponen&nbsp;(
+            <a href="https://twitter.com/anttilip" target="_blank" rel="noopener noreferrer">@anttilip</a>)&nbsp;
+            <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank" rel="noopener noreferrer">CC-BY</a>
+          </small></p>
 
+        </TextBox>
 
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Infrastructure Sector</TableCell>
-            <TableCell>Assets</TableCell>
-            <TableCell>Exposure</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>Transport</TableCell>
-            <TableCell>Road links</TableCell>
-            <TableCell>Lengths exposed to coastal or river flooding</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Energy</TableCell>
-            <TableCell>Electricity generation (power stations) and transmission lines</TableCell>
-            <TableCell>Lengths exposed to high wind speeds from tropical cyclones</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
-
-    <p>For more details on the infrastructure and hazard data used, see
-    the <Link to="/data">Data page</Link></p>
-
-    <h2>Open-source code</h2>
-
-    <p>This tool to visualize the model outputs is developed and documented
-    here:</p>
-
-    <ul>
-      <li>
-        <a href="https://github.com/nismod/infra-risk-vis" target="blank">
-          github.com/nismod/infra-risk-vis
-        </a>
-      </li>
-    </ul>
-
-    <p>The analytics for the Caribbean are produced using the code and models here:</p>
-
-    <ul>
-      <li>
-        <a href="https://github.com/nismod/open-gira" target="blank">
-          github.com/nismod/open-gira
-        </a>
-      </li>
-    </ul>
-
-    <h2>Funding and support</h2>
-
-    <p>This research is led by researchers in the Oxford Programme for
-    Sustainable Infrastructure Systems in the Environmental Change Institute,
-    University of Oxford as part of a project funded by UK Aid (FCDO).</p>
-
-    <Stack
-      direction="row"
-      divider={<Divider orientation="vertical" flexItem />}
-      spacing={2}
-      justifyContent="center"
-      alignItems="center"
-    >
-      <a href="https://opsis.eci.ox.ac.uk" target="_blank" rel="noopener noreferrer">
-        <img height="100" src="/logo-opsis.png" alt="Oxford Programme for Sustainable Infrastructure Systems" />
-      </a>
-      <a href="https://www.gov.uk/guidance/uk-aid" target="_blank" rel="noopener noreferrer">
-        <img height="100" src="/logo-ukaid.png" alt="UK AID" />
-      </a>
-    </Stack>
+      </Grid>
+    </Grid>
   </article>
 );
