d6cc8bb786a2bfc309161f0422406eabd754126e
Tom Russell <tomalrussell@gmail.com>
2022-07-29 18:13:25 +0100

    Update intro text, refer to Jamaica

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -27,7 +27,7 @@ export const IntroPage = () => (
         <HeadingBox sx={{mt:-2, pt:8}}>
           <Typography variant="h1">
 
-            Climate-related risk analytics for transport, energy &amp; water
+            Climate-related risk analytics for transport &amp; energy
             infrastructure in the Caribbean
 
           </Typography>
@@ -50,11 +50,13 @@ export const IntroPage = () => (
       <Grid item xs={12}>
         <TextBox sx={{backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
 
-          <p>The research, analysis and development has been led by
+        <p>The research, analysis and development has been led by
             researchers in the&nbsp;<a href="https://opsis.eci.ox.ac.uk/" target="_blank" rel="noopener noreferrer">Oxford Programme for
-            Sustainable Infrastructure Systems</a>, University of Oxford,
-            funded by UK Aid through the UK Foreign and
-            Commonwealth Development Office (FCDO)</p>
+            Sustainable Infrastructure Systems</a>, University of Oxford, as
+            part of a project largely focussed on <a href="https://jamaica.infrastructureresilience.org/" target="_blank" rel="noopener noreferrer">Jamaica</a>, in collaboration with
+            the Planning Institute of Jamaica and supported by engagement with
+            infrastructure and climate specialists and related government
+            bodies.</p>
           <p>
           <Stack
             direction="row"
@@ -64,11 +66,47 @@ export const IntroPage = () => (
             alignItems="center"
             spacing={2}
             >
+            <a href="https://www.gov.jm" target="_blank" rel="noopener noreferrer">
+              <img height="150" src="/jamaica-coatofarms.png" alt="Government of Jamaica" />
+            </a>
+            <a href="https://www.pioj.gov.jm/" target="_blank" rel="noopener noreferrer">
+              <img height="150" src="/jamaica-pioj.png" alt="Planning Institute of Jamaica" />
+            </a>
             <a href="https://opsis.eci.ox.ac.uk" target="_blank" rel="noopener noreferrer">
               <img height="100" src="/logo-opsis.png" alt="Oxford Programme for Sustainable Infrastructure Systems" />
             </a>
+          </Stack>
+          </p>
+
+
+          <Typography variant="h2">Funding and support</Typography>
+
+          <p>This project is led by researchers in the&nbsp;
+            <a href="https://opsis.eci.ox.ac.uk/" target="_blank" rel="noopener noreferrer">Oxford Programme for
+            Sustainable Infrastructure Systems</a> in the Environmental Change
+            Institute, University of Oxford, with the Government of
+            Jamaica (GoJ), funded by UK Aid through the UK Foreign and
+            Commonwealth Development Office (FCDO). The initiative forms part
+            of the Coalition for Climate Resilient Investment&rsquo;s (CCRI)
+            work on "Systemic Resilience" in collaboration with the Green
+            Climate Fund.</p>
+
+          <p>
+          <Stack
+            direction="row"
+            divider={<Divider orientation="vertical" flexItem />}
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
             <a href="https://www.gov.uk/guidance/uk-aid" target="_blank" rel="noopener noreferrer">
               <img height="100" src="/logo-ukaid.png" alt="UK AID" />
+            </a>
+            <a href="https://www.greenclimate.fund/" target="_blank" rel="noopener noreferrer">
+              <img height="100" src="/logo-gcf.png" alt="Green Climate Fund" />
+            </a>
+            <a href="https://resilientinvestment.org/" target="_blank" rel="noopener noreferrer">
+              <img height="100" src="/logo-ccri.png" alt="Coalition for Climate Resilient Investment" />
             </a>
           </Stack>
           </p>
