ff562fc718f870cbaa76f37abf5ae67c91d5a0cc
Tom Russell <tomalrussell@gmail.com>
2022-08-04 22:27:55 +0100

    Drop damageType from damage style selection

--- src/sidebar/networks/DamageSourceControl.tsx
+++ src/sidebar/networks/DamageSourceControl.tsx
@@ -2,10 +2,8 @@ import {
   FormControl,
   FormControlLabel,
   FormLabel,
-  MenuItem,
   Radio,
   RadioGroup,
-  Select,
 } from '@mui/material';
 import { useRecoilState } from 'recoil';
 
@@ -15,27 +13,17 @@ import { InputSection } from 'sidebar/ui/InputSection';
 import { InputRow } from 'sidebar/ui/InputRow';
 import { EpochControl } from 'sidebar/ui/params/EpochControl';
 import { RCPControl } from 'sidebar/ui/params/RCPControl';
-import { damageSourceState, damageSourceStateEffect, damageTypeState } from 'state/damage-mapping/damage-map';
+import { damageSourceState, damageSourceStateEffect } from 'state/damage-mapping/damage-map';
 import { HAZARDS_METADATA, HAZARDS_UI_ORDER } from 'config/hazards/metadata';
 import { LayerStylePanel } from 'sidebar/ui/LayerStylePanel';
 
 export const DamageSourceControl = () => {
   const [damageSource, setDamageSource] = useRecoilState(damageSourceState);
-  const [damageType, setDamageType] = useRecoilState(damageTypeState);
 
   return (
     <>
       <StateEffectRoot state={damageSourceState} effect={damageSourceStateEffect} />
       <LayerStylePanel>
-        <InputSection>
-          <FormControl fullWidth>
-            <FormLabel>Damage type</FormLabel>
-            <Select<string> variant="standard" value={damageType} onChange={(e) => setDamageType(e.target.value)}>
-              <MenuItem value="direct">Direct Damages</MenuItem>
-              <MenuItem value="indirect">Economic Losses</MenuItem>
-            </Select>
-          </FormControl>
-        </InputSection>
         <InputSection>
           <FormControl>
             <FormLabel>Hazard</FormLabel>
