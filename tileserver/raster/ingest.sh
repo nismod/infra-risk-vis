16163eceeaf5c58d49c2069443400ca51ddd7267
Tom Russell <tomalrussell@gmail.com>
2022-09-16 20:52:58 +0100

    Skip _conf in hazard tiff ingest

--- tileserver/raster/ingest.sh
+++ tileserver/raster/ingest.sh
@@ -3,5 +3,5 @@
 RASTER_BASE_PATH=$1
 
 terracotta ingest \
-  "$RASTER_BASE_PATH/data/{type}__rp_{rp}__rcp_{rcp}__epoch_{epoch}__conf_{confidence}.tif" \
+  "$RASTER_BASE_PATH/data/{type}__rp_{rp}__rcp_{rcp}__epoch_{epoch}.tif" \
   -o "$RASTER_BASE_PATH/terracotta.sqlite"
