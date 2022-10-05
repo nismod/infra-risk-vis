ba5c4250beb4fd3b45cd89b933325533e439496b
Your Name <you@example.com>
2022-10-05 11:09:37 +0100

    demo tiles from new backend tileserver

--- containers/web-server/frontend/src/config/hazards/source.ts
+++ containers/web-server/frontend/src/config/hazards/source.ts
@@ -1,7 +1,7 @@
 export const HAZARD_SOURCE = {
   getDataUrl({ hazardType, hazardParams: { returnPeriod, rcp, epoch, gcm } }, { scheme, range }) {
     const sanitisedRcp = rcp.replace('.', 'x');
-
-    return `/raster/singleband/${hazardType}/${returnPeriod}/${sanitisedRcp}/${epoch}/${gcm}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
+    // TODO: Swap this to parse the meta from API about DB source instead of hardcoded
+    return `/raster/singleband/aqueduct/${hazardType}/${returnPeriod}/${sanitisedRcp}/${epoch}/${gcm}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
   },
 };
