0bc9c686773da7019a96f4aeeca6eac75bde6379
Your Name <you@example.com>
2022-10-06 20:40:34 +0100

    redirect tileserver to API endpoint

--- containers/web-server/frontend/src/config/hazards/source.ts
+++ containers/web-server/frontend/src/config/hazards/source.ts
@@ -1,6 +1,6 @@
 export const HAZARD_SOURCE = {
   getDataUrl({ hazardType, hazardParams: { returnPeriod, rcp, epoch, gcm } }, { scheme, range }) {
     const sanitisedRcp = rcp.replace('.', 'x');
-    return `/raster/tiles/${hazardType}/${returnPeriod}/${sanitisedRcp}/${epoch}/${gcm}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
+    return `/api/tiles/${hazardType}/${returnPeriod}/${sanitisedRcp}/${epoch}/${gcm}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
   },
 };
