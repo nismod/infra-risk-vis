99ba64d3ed0f3c8a223a69503e5383cc7279e1bb
Your Name <you@example.com>
2022-10-06 11:37:02 +0100

    revert url generation to original

--- containers/web-server/frontend/src/config/hazards/source.ts
+++ containers/web-server/frontend/src/config/hazards/source.ts
@@ -1,7 +1,6 @@
 export const HAZARD_SOURCE = {
   getDataUrl({ hazardType, hazardParams: { returnPeriod, rcp, epoch, gcm } }, { scheme, range }) {
     const sanitisedRcp = rcp.replace('.', 'x');
-    // TODO: Swap this to parse the meta from API about DB source instead of hardcoded
-    return `/raster/singleband/aqueduct/${hazardType}/${returnPeriod}/${sanitisedRcp}/${epoch}/${gcm}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
+    return `/raster/tiles/${hazardType}/${returnPeriod}/${sanitisedRcp}/${epoch}/${gcm}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
   },
 };
