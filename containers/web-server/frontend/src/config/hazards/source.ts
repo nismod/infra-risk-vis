1dd8c19a7e51da54242e0110d2f1fe98365ca2b0
Your Name <you@example.com>
2022-10-11 17:12:00 +0100

    added temporary switch for cyclone and heat

--- containers/web-server/frontend/src/config/hazards/source.ts
+++ containers/web-server/frontend/src/config/hazards/source.ts
@@ -1,6 +1,15 @@
 export const HAZARD_SOURCE = {
   getDataUrl({ hazardType, hazardParams: { returnPeriod, rcp, epoch, gcm } }, { scheme, range }) {
     const sanitisedRcp = rcp.replace('.', 'x');
-    return `/api/tiles/${hazardType}/${returnPeriod}/${sanitisedRcp}/${epoch}/${gcm}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
+    // TODO: Gather required keys from API
+    if (hazardType === 'cyclone') {
+      return `/api/tiles/${hazardType}/${returnPeriod}/${gcm}//{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
+    }
+    else if (hazardType === 'extreme_heat') {
+      // TODO: Add support for exposure metric (as well as occurrence)
+      return `/api/tiles/${hazardType}/occurrence/${sanitisedRcp}/${gcm}//{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
+    } else {
+      return `/api/tiles/${hazardType}/${returnPeriod}/${sanitisedRcp}/${epoch}/${gcm}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`;
+    }
   },
 };
