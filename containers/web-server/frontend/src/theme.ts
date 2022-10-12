386cee51792cb84f115cd3594be7729db1fb96fe
Your Name <you@example.com>
2022-10-12 08:23:21 +0100

    theme update

--- containers/web-server/frontend/src/theme.ts
+++ containers/web-server/frontend/src/theme.ts
@@ -3,7 +3,7 @@ import '@mui/styles';
 
 declare module '@mui/styles/defaultTheme' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
+  interface DefaultTheme extends Theme { }
 }
 
 export const theme = createTheme({
@@ -12,6 +12,44 @@ export const theme = createTheme({
       main: '#3f51b5',
     },
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Catamaran';
+          src: url('/fonts/static/catamaran-regular.ttf');
+          font-weight: normal;
+        }
+        @font-face {
+          font-family: 'Catamaran';
+          src: url('/fonts/static/catamaran-semibold.ttf');
+          font-weight: 600;
+        }
+        @font-face {
+          font-family: 'Catamaran';
+          src: url('/fonts/static/catamaran-bold.ttf');
+          font-weight: bold;
+        }
+      `
+    }
+  },
+  typography: {
+    h1: {
+      fontWeight: 600,
+      fontFamily: 'Catamaran, Roboto, sans-serif',
+      fontSize: '2.75rem'
+    },
+    h2: {
+      fontWeight: 600,
+      fontFamily: 'Catamaran, Roboto, sans-serif',
+      fontSize: '1.6rem'
+    },
+    h6: {
+      fontWeight: 600,
+      fontFamily: 'Catamaran, Roboto, sans-serif',
+      fontSize: '1.6rem'
+    }
+  }
 });
 
 export const globalStyleVariables = {
