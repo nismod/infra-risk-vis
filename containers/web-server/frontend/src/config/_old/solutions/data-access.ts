1fc92c03f2bdf73ef4bcfeaa7526131f26083019
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-11-06 16:13:00 +0100

    Remove code for old, unused data layers

--- containers/web-server/frontend/src/config/_old/solutions/data-access.ts
+++
@@ -1,7 +0,0 @@
-import { FieldSpec } from '@/lib/data-map/view-layers';
-import { featureProperty } from '@/lib/deck/props/data-source';
-import { Accessor } from '@/lib/deck/props/getters';
-
-export function getSolutionsDataAccessor(fieldSpec: FieldSpec): Accessor<any> {
-  return fieldSpec && featureProperty(fieldSpec.field);
-}
