ad29a3161b353d9b786b3e4351e20d382d4de789
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-11-07 20:52:25 +0100

    Add metric parameter to hazards source url

--- containers/web-server/frontend/src/config/hazards/source.ts
+++ containers/web-server/frontend/src/config/hazards/source.ts
@@ -1,37 +1,41 @@
+import { RasterColorMap } from '@/map/legend/RasterLegend';
+
 import { SOURCES } from '../sources';
 
-export const HAZARD_SOURCE = {
-  getDataUrl({ hazardType, hazardParams }, { scheme, range }) {
-    let path: string;
-    // TODO: Gather required keys from API
-    if (hazardType === 'earthquake') {
-      const { rp, medium } = hazardParams;
-      path = `earthquake/${rp}/${medium}`;
+export interface HazardSourceParams {
+  hazardType: string;
+  hazardParams: any;
+  metric: 'occurrence' | 'exposure';
+}
+
+export function getHazardDataPath({ hazardType, hazardParams, metric }: HazardSourceParams) {
+  let path: string;
+  // TODO: Gather required keys from API
+  if (hazardType === 'earthquake') {
+    const { rp, medium } = hazardParams;
+    path = `earthquake/${rp}/${medium}`;
+  } else {
+    const { rp, rcp, epoch, gcm } = hazardParams;
+    const sanitisedRcp = rcp?.replace('.', 'x');
+
+    if (hazardType === 'cyclone') {
+      path = `${hazardType}/${rp}/${gcm}`;
+    } else if (['extreme_heat', 'drought'].includes(hazardType)) {
+      path = `${hazardType}/${metric}/${sanitisedRcp}/${epoch}/${gcm}`;
     } else {
-      const { rp, rcp, epoch, gcm } = hazardParams;
-      const sanitisedRcp = rcp?.replace('.', 'x');
-
-      if (hazardType === 'cyclone') {
-        path = `${hazardType}/${rp}/${gcm}`;
-      } else if (hazardType === 'extreme_heat') {
-        // TODO: Add support for exposure metric (as well as occurrence)
-        path = `extreme_heat/occurrence/${sanitisedRcp}/${epoch}/${gcm}`;
-        /*
-        else if (hazardType === 'extreme_heat_exposure') {
-          path = `extreme_heat/exposure/${sanitisedRcp}/${epoch}/${gcm}`;
-        }
-        */
-      } else if (hazardType === 'drought') {
-        path = `drought/occurrence/${sanitisedRcp}/${epoch}/${gcm}`;
-      } else {
-        path = `${hazardType}/${rp}/${sanitisedRcp}/${epoch}/${gcm}`;
-      }
+      path = `${hazardType}/${rp}/${sanitisedRcp}/${epoch}/${gcm}`;
     }
+  }
 
-    return SOURCES.raster.getUrl({
-      path,
-      scheme,
-      range,
-    });
-  },
-};
+  return path;
+}
+
+export function getHazardDataUrl(params: HazardSourceParams, { scheme, range }: RasterColorMap) {
+  const path = getHazardDataPath(params);
+
+  return SOURCES.raster.getUrl({
+    path,
+    scheme,
+    range,
+  });
+}
--- containers/web-server/frontend/src/config/sources.ts
+++ containers/web-server/frontend/src/config/sources.ts
@@ -1,7 +1,19 @@
+function paramString(params: string[]) {
+  return params.length ? `?${params.join('&')}` : '';
+}
+
 export const SOURCES = {
   raster: {
-    getUrl: ({ path, scheme, range }) =>
-      `/api/tiles/${path}/{z}/{x}/{y}.png?colormap=${scheme}&stretch_range=[${range[0]},${range[1]}]`,
+    getUrl: ({ path, scheme, range }) => {
+      const params = [];
+      if (scheme) {
+        params.push(`colormap=${scheme}`);
+      }
+      if (range) {
+        params.push(`stretch_range=[${range[0]},${range[1]}]`);
+      }
+      return `/api/tiles/${path}/{z}/{x}/{y}.png${paramString(params)}`;
+    },
   },
   vector: {
     getUrl: (datasetId) => `/vector/data/${datasetId}.json`,
