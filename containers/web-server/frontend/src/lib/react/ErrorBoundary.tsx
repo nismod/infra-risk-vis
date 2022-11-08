310a0cb54e9ccb9ba5e021d348f4aa0a6ec6b0ba
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2022-11-08 15:23:21 +0100

    Add development-only UI to ErrorBoundary

--- containers/web-server/frontend/src/lib/react/ErrorBoundary.tsx
+++ containers/web-server/frontend/src/lib/react/ErrorBoundary.tsx
@@ -1,10 +1,11 @@
 import { ErrorOutline } from '@mui/icons-material';
-import { Stack, StackProps, Typography } from '@mui/material';
+import { Paper, Stack, StackProps, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { Component } from 'react';
 
 interface ErrorBoundaryState {
   hasError: boolean;
+  error: Error;
 }
 
 interface ErrorBoundaryProps {
@@ -15,21 +16,34 @@ interface ErrorBoundaryProps {
 export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: null };
   }
 
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true, error };
   }
   componentDidCatch(error, errorInfo) {}
   render() {
     if (this.state.hasError) {
       return (
         <Box p={1}>
-          <Stack direction="row" alignItems="center" justifyContent={this.props.justifyErrorContent} gap={1}>
-            <ErrorOutline />
-            <Typography>{this.props.message}</Typography>
+          <Stack direction="column" spacing={2}>
+            <Stack direction="row" alignItems="center" justifyContent={this.props.justifyErrorContent} gap={1}>
+              <ErrorOutline />
+              <Typography>{this.props.message}</Typography>
+            </Stack>
+            {process.env.NODE_ENV === 'development' && (
+              <Box maxWidth="min(100%,700px)" alignSelf="center">
+                <Paper>
+                  <Box p={1} maxWidth="100%" overflow="scroll">
+                    <pre>
+                      <code>{this.state.error?.stack.toString() ?? this.state.error.toString()}</code>
+                    </pre>
+                  </Box>
+                </Paper>
+              </Box>
+            )}
           </Stack>
         </Box>
       );
