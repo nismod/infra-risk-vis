259daf279cd6d9811a98fa57105a899d08fe750e
Tom Russell <tom.russell@ouce.ox.ac.uk>
2022-11-29 10:45:10 +0000

    Proxy to server for frontend-only local dev

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,34 +9,58 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+  // /* Full local development */
+  // app.use(
+  //   '/vector',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:8080',
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       '^/vector': '/', // remove base path
+  //     },
+  //   }),
+  // );
+  // app.use(
+  //   '/raster',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:5000',
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       '^/raster': '/', // remove base path
+  //     },
+  //   }),
+  // );
+  // app.use(
+  //   '/api',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:8888',
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       '^/api': '/', // remove base path
+  //     },
+  //   }),
+  // );
+
+  /* Frontend only local development, proxy to server */
   app.use(
     '/vector',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'https://east-africa.infrastructureresilience.org',
       changeOrigin: true,
-      pathRewrite: {
-        '^/vector': '/', // remove base path
-      },
     }),
   );
   app.use(
     '/raster',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://east-africa.infrastructureresilience.org',
       changeOrigin: true,
-      pathRewrite: {
-        '^/raster': '/', // remove base path
-      },
     }),
   );
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8888',
+      target: 'https://east-africa.infrastructureresilience.org',
       changeOrigin: true,
-      pathRewrite: {
-        '^/api': '/', // remove base path
-      },
     }),
   );
 };
