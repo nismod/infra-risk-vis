062e12b7fe8037ecf9b03bdea832ff3cc972df7d
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2023-02-07 09:21:10 +0100

    Add mobile bottom sheet tabs content

--- containers/web-server/frontend/src/pages/MapPage.tsx
+++ containers/web-server/frontend/src/pages/MapPage.tsx
@@ -1,7 +1,7 @@
 import { Layers, Palette, TableRows } from '@mui/icons-material';
 import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
-import { FC } from 'react';
-import { BottomSheet } from 'react-spring-bottom-sheet';
+import { FC, useRef, useState } from 'react';
+import { BottomSheet, BottomSheetRef } from 'react-spring-bottom-sheet';
 
 import { ErrorBoundary } from '@/lib/react/ErrorBoundary';
 import { StateEffectRoot } from '@/lib/recoil/state-effects/StateEffectRoot';
@@ -10,6 +10,7 @@ import { useSyncRecoilState } from '@/lib/recoil/sync-state';
 import { InitData } from '@/InitData';
 import { DetailsContent } from '@/details/DetailsContent';
 import { MapView } from '@/map/MapView';
+import { MapLegend } from '@/map/legend/MapLegend';
 import { LayersSidebar } from '@/sidebar/LayersSidebar';
 import { viewState, viewStateEffect } from '@/state/view';
 import { globalStyleVariables } from '@/theme';
@@ -72,34 +73,72 @@ const MapPageDesktopLayout = () => {
   );
 };
 
-const MapPageMobileLayout = () => (
-  <>
-    <Box position="absolute" overflow="clip" top={0} left={0} right={0} bottom={0}>
-      <MapView />
-    </Box>
-    <BottomSheet
-      open={true}
-      blocking={false}
-      snapPoints={({ footerHeight, maxHeight }) => [
-        footerHeight + 38, // magic number to allow the puller/header to be visible at the smallest snap point
-        maxHeight * 0.5,
-        maxHeight * 0.9,
-      ]}
-      footer={
-        <BottomNavigation showLabels sx={{ padding: 0 }}>
-          <BottomNavigationAction label="Layers" icon={<Layers fontSize="small" />} />
-          <BottomNavigationAction label="Legend" icon={<Palette fontSize="small" />} />
-          <BottomNavigationAction label="Selection" icon={<TableRows fontSize="small" />} />
-        </BottomNavigation>
-      }
-      header={<Box height={10} />}
-    >
-      <Box m={2}>
-        <LayersSidebar />
+const MapPageMobileLayout = () => {
+  const [bottomTab, setBottomTab] = useState(0);
+  const sheetRef = useRef<BottomSheetRef>();
+
+  /*
+  const handleChange = useCallback(() => {
+    if (sheetRef.current.height <= 100) {
+      sheetRef.current.snapTo(({ snapPoints }) => snapPoints[1]);
+    }
+  }, []);
+  */
+
+  const tabs = [
+    {
+      label: 'Layers',
+      icon: <Layers fontSize="small" />,
+      content: <LayersSidebar key="layers" />,
+    },
+    {
+      label: 'Legend',
+      icon: <Palette fontSize="small" />,
+      content: <MapLegend key="legend" />,
+    },
+    {
+      label: 'Selection',
+      icon: <TableRows fontSize="small" />,
+      content: <DetailsContent key="details" />,
+    },
+  ];
+
+  return (
+    <>
+      <Box position="absolute" overflow="clip" top={0} left={0} right={0} bottom={0}>
+        <MapView />
       </Box>
-    </BottomSheet>
-  </>
-);
+      <BottomSheet
+        ref={sheetRef}
+        open={true}
+        blocking={false}
+        expandOnContentDrag={true}
+        skipInitialTransition={true}
+        snapPoints={({ footerHeight, maxHeight }) => [
+          footerHeight + 38, // magic number to allow the puller/header to be visible at the smallest snap point
+          maxHeight * 0.35,
+          maxHeight * 0.6,
+          maxHeight - globalStyleVariables.navbarHeight - 4,
+        ]}
+        footer={
+          <BottomNavigation
+            showLabels
+            // sx={{ padding: 0 }}
+            value={bottomTab}
+            onChange={(e, value) => setBottomTab(value)}
+          >
+            {tabs.map(({ label, icon }) => (
+              <BottomNavigationAction key={label} label={label} icon={icon} />
+            ))}
+          </BottomNavigation>
+        }
+        header={<Box height={10} />}
+      >
+        <Box m={2}>{tabs[bottomTab].content}</Box>
+      </BottomSheet>
+    </>
+  );
+};
 
 const MapPageLayout = () => {
   const isMobile = useIsMobile();
