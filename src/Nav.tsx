8791ac7a2150f505b9f1d379476ecec404aea57e
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2023-03-28 13:21:03 +0200

    Nav bar tweaks

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Tooltip, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 export const Nav = () => {
@@ -19,22 +19,36 @@ export const Nav = () => {
         <NavLink exact className="nav-link" to="/">
           <Typography variant="h6">J-SRAT</Typography>
         </NavLink>
-        <NavLink className="nav-link" to="/exposure">
-          <Typography variant="h6">Exposure</Typography>
-        </NavLink>
-        <NavLink className="nav-link" to="/risk">
-          <Typography variant="h6">Risk</Typography>
-        </NavLink>
-        <NavLink className="nav-link" to="/adaptation">
-          <Typography variant="h6">Adaptation</Typography>
-        </NavLink>
-        <NavLink className="nav-link" to="/nature-based-solutions">
-          <Typography variant="h6">Nature-based Solutions</Typography>
-        </NavLink>
-        <NavLink className="nav-link" to="/data">
-          <Typography variant="h6">Data</Typography>
-        </NavLink>
+        <NavTooltip title="Infrastructure assets and natural hazards">
+          <NavLink className="nav-link" to="/exposure">
+            <Typography variant="h6">Exposure</Typography>
+          </NavLink>
+        </NavTooltip>
+        <NavTooltip title="Risk of hazard-related damages to assets">
+          <NavLink className="nav-link" to="/risk">
+            <Typography variant="h6">Risk</Typography>
+          </NavLink>
+        </NavTooltip>
+        <NavTooltip title="Adaptation options to decrease hazard-related risks">
+          <NavLink className="nav-link" to="/adaptation">
+            <Typography variant="h6">Adaptation</Typography>
+          </NavLink>
+        </NavTooltip>
+        <NavTooltip title="Analysis of nature-based solutions potential">
+          <NavLink className="nav-link" to="/nature-based-solutions">
+            <Typography variant="h6">Nature-based Solutions</Typography>
+          </NavLink>
+        </NavTooltip>
+        <NavTooltip title="More information about datasets in the tool">
+          <NavLink className="nav-link" to="/data">
+            <Typography variant="h6">About</Typography>
+          </NavLink>
+        </NavTooltip>
       </Toolbar>
     </AppBar>
   );
 };
+
+function NavTooltip({ ...props }: any) {
+  return <Tooltip enterDelay={500} {...props} />;
+}
