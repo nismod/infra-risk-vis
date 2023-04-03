9e2e4ff9a303fe22c42f75c0e9144257f6169553
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2023-04-03 16:55:50 +0200

    Add data license notice in bottom drawer

--- src/Notice.tsx
+++ src/Notice.tsx
@@ -1,8 +1,10 @@
+import { InfoOutlined } from '@mui/icons-material';
+import { Box, Button, DialogActions, Drawer, Stack, Typography } from '@mui/material';
+import { date, nullable } from '@recoiljs/refine';
+import { AppLink } from 'lib/nav';
+import { useCallback } from 'react';
 import { atom, useRecoilState } from 'recoil';
 import { syncEffect } from 'recoil-sync';
-import { date, optional } from '@recoiljs/refine';
-import { Button, Dialog, DialogActions, Typography } from '@mui/material';
-import { useCallback } from 'react';
 
 const noticeAcceptedDateState = atom<Date | null>({
   key: 'noticeAcceptedDate',
@@ -11,7 +13,7 @@ const noticeAcceptedDateState = atom<Date | null>({
     syncEffect({
       storeKey: 'local-storage',
       itemKey: 'notice-accepted',
-      refine: optional(date()),
+      refine: nullable(date()),
     }),
   ],
 });
@@ -27,15 +29,23 @@ export const Notice = () => {
   );
 
   return (
-    <Dialog open={acceptedDate == null}>
-      <Typography paragraph>
-        The systemic risk analysis results shown in this tool contain licensed data that must not be shared outside the
-        Government of Jamaica. By accessing the tool, you acknowledge that you understand this and agree not to download
-        any data or share your access credentials with anyone else.
-      </Typography>
-      <DialogActions>
-        <Button onClick={handleAccept}>Accept</Button>
-      </DialogActions>
-    </Dialog>
+    <Drawer variant="persistent" anchor="bottom" open={acceptedDate == null}>
+      <Stack direction="row" justifyContent="space-between" mx={1} my={0.5}>
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Box>
+            <InfoOutlined color="primary" />
+          </Box>
+          <Typography paragraph>
+            The systemic risk analysis results shown in this tool contain licensed data that must not be shared outside
+            the Government of Jamaica. By accessing the tool, you acknowledge that you understand this and agree not to
+            download any data or share your access credentials with anyone else.{' '}
+            <AppLink to="/data">Read more about the data</AppLink>
+          </Typography>
+        </Stack>
+        <DialogActions>
+          <Button onClick={handleAccept}>Accept</Button>
+        </DialogActions>
+      </Stack>
+    </Drawer>
   );
 };
