0ef44d51496478b399f6f20a153b393d1684e876
Tom Russell <tom.russell@ouce.ox.ac.uk>
2024-12-02 13:30:06 +0000

    Update to sqlalchemy select API and FastAPI SessionDep

--- containers/backend/backend/app/routers/features.py
+++ containers/backend/backend/app/routers/features.py
@@ -3,28 +3,27 @@ from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi_pagination import Page, Params
 from fastapi_pagination.ext.sqlalchemy import paginate
-from sqlalchemy import desc
+from sqlalchemy import desc, select
 from sqlalchemy.exc import NoResultFound
-from sqlalchemy.orm import Session
 from geoalchemy2 import functions
 
 from backend.app import schemas
-from backend.app.dependencies import get_db
 from backend.app.internal.attribute_access import (
     add_value_query,
     parse_dimensions,
     parse_parameters,
 )
 from backend.db import models
+from backend.db.database import SessionDep
 
 
 router = APIRouter(tags=["features"])
 
 
 @router.get("/{feature_id}", response_model=schemas.FeatureOut)
-def read_feature(feature_id: int, db: Session = Depends(get_db)):
+def read_feature(feature_id: int, session: SessionDep):
     try:
-        feature = db.query(models.Feature).filter(models.Feature.id == feature_id).one()
+        feature = session.get(models.Feature, feature_id)
     except NoResultFound:
         raise HTTPException(
             status_code=404,
@@ -53,15 +52,15 @@ def get_layer_spec(
 def read_sorted_features(
     field_group: str,
     field: str,
+    session: SessionDep,
     field_dimensions: schemas.DataDimensions = Depends(parse_dimensions),
     field_params: schemas.DataParameters = Depends(parse_parameters),
     layer_spec: schemas.LayerSpec = Depends(get_layer_spec),
     page_params: Params = Depends(),
-    db: Session = Depends(get_db),
 ):
     filled_layer_spec = {k: v for k, v in layer_spec.dict().items() if v is not None}
     base_query = (
-        db.query(
+        select(
             models.Feature.id.label("id"),
             models.Feature.string_id.label("string_id"),
             models.Feature.layer.label("layer"),
@@ -76,4 +75,4 @@ def read_sorted_features(
         base_query, field_group, field_dimensions, field, field_params
     ).order_by(desc("value"))
 
-    return paginate(q, page_params)
+    return paginate(session, q, page_params)
