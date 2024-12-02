46efb9f3e4595332bfb7cb7d8c313b9b687dc25a
Tom Russell <tom.russell@ouce.ox.ac.uk>
2024-12-02 11:43:35 +0000

    Require stretch_range argument for colormap

--- containers/backend/backend/app/routers/colormap.py
+++ containers/backend/backend/app/routers/colormap.py
@@ -4,7 +4,6 @@ Colormap
 
 import inspect
 import json
-from typing import Optional
 
 from fastapi import APIRouter, HTTPException
 from fastapi.logger import logger
@@ -29,7 +28,7 @@ def _get_colormap(options: schemas.ColorMapOptions) -> schemas.ColorMap:
 @router.get("", response_model=schemas.ColorMap)
 async def get_colormap(
     colormap: str,
-    stretch_range: Optional[str],
+    stretch_range: str,
     num_values: int = 255,
 ) -> schemas.ColorMap:
     """
@@ -43,10 +42,15 @@ async def get_colormap(
 
     ::kwarg num_values int Number of values to generate in the colormap
     """
-    if stretch_range is None:
-        stretch_range_arg: list[int] = [0, 10]
-    else:
-        stretch_range_arg = json.loads(stretch_range)
+    try:
+        stretch_range_arg: list[int] = json.loads(stretch_range)
+        if len(stretch_range_arg) != 2:
+            raise ValueError
+    except ValueError:
+        raise HTTPException(
+            status_code=400,
+            detail="stretch_range should be a list of two values (min and max), like `stretch_range=[0,1]`",
+        )
 
     logger.debug(
         "performing %s, using %s, %s, %s",
